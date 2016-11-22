c934876dbca842837d6ff551e3e777f3f62540c6
Luke Gerber <lucas.a.gerber@gmail.com>
2016-11-22 16:33:42 -0500

    Draft as of 11/22/16

---
+++ __init__.py
@@ -0,0 +1,10 @@
+"""
+kalamatchkas
+Lucas A. Gerber
+Description:  program writes out random recipes based on dietary restrictions
+"""
+
+
+from .recipe import *
+from .diet import *
+from .config import *
---
+++ config.py
@@ -0,0 +1,14 @@
+"""
+kalamatchkas.config
+Lucas A. Gerber
+"""
+
+
+# file of foods to load
+FOOD_PATH = "C:/Users/Lucas/Desktop/doron_diet_program/sample/ingredient_sample.xlsx"
+
+# file to output recipes
+OUT_PATH = "C:/Users/Lucas/Desktop/doron_diet_program/sample/test_out.csv"
+
+# string used to begin each printed line
+LINE_BEGIN = ">*~@~*> "
